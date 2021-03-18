f42f91ac4371dd8fec32d3d8e366a665eb27ba06
Travis Musall <tamusall@gmail.com>
2021-03-18 19:21:44 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Travis Musall on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
